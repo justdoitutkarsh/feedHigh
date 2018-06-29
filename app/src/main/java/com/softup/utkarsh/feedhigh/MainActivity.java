b0e47f4ba209a1b32fd5076bcbc4abce9423dba1
justdoitutkarsh <justdoitutkarsh@gmail.com>
2018-06-29 03:59:36 -0700

    login page

--- app/src/main/java/com/softup/utkarsh/feedhigh/MainActivity.java
+++ app/src/main/java/com/softup/utkarsh/feedhigh/MainActivity.java
@@ -1,15 +1,27 @@
 package com.softup.utkarsh.feedhigh;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
 
+import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
+import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
+
 public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        CalligraphyConfig.initDefault(new CalligraphyConfig.Builder()
+                                        .setDefaultFontPath("fonts/Arkhip_font.ttf")
+                                        .setFontAttrId(R.attr.fontPath)
+                                        .build());
         setContentView(R.layout.activity_main);
         //utkarsh
         //pandey
